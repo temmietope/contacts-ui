dec011873c20bf5e32d586f397c1e240ed9caf94
Temitope Ayodele <39211865+temmietope@users.noreply.github.com>
2019-12-12 13:16:58 +0100

    first commit

---
+++ src/components/Contacts/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="contacts-wrapper">
+hellop
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: "Contacts",
+  components: {
+    // Home
+  }
+};
+</script>
+
+<style>
+.contacts-wrapper {
+  
+}
+</style>
